e4d29240da93881e0c4fb4960156111ed558e1eb
maku693 <me@maku693.net>
2021-03-31 01:38:50 +0900

    first commit

---
+++ gen.go
@@ -0,0 +1,3 @@
+package main
+
+//go:generate go run github.com/hairyhenderson/gomplate/v3/cmd/gomplate --datasource source=animal.yml --file enum.go.tmpl --out animal_gen.go
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/maku693/gomplate-example
+
+go 1.16
+
+require github.com/hairyhenderson/gomplate/v3 v3.9.0
